111976cdc52cf1223973d80cfb817390794328f4
Gabivaltrick15 <gabi.taronho@gmail.com>
2025-10-23 18:02:04 -0300

    Create bot_dc.py
    client.run("chave do seu bot do discord")

---
+++ bot_dc.py
@@ -0,0 +1,20 @@
+import discord
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+@client.event
+async def on_ready():
+    print(f'Fizemos login como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hello!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("your bot's Discord key")
